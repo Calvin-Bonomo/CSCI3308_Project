fa73b932a2c1098d3bc3fd597a788b664721951a
Jackson Hill <jahi7479@colorado.edu>
2024-11-06 14:23:29 -0700

    Added interview sql table

---
+++ ProjectSourceCode/InitData/interview_db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE interviewers
+(
+  interviewer_id SERIAL PRIMARY KEY,          -- Unique identifier for each interviewer
+  name VARCHAR(100) NOT NULL,                 -- Full name of the interviewer
+  position VARCHAR(100) NOT NULL,             -- Position or job title of the interviewer
+  email VARCHAR(100) UNIQUE NOT NULL,         -- Email address (unique for each interviewer)
+  phone_number VARCHAR(15),                   -- Contact phone number of the interviewer
+  misc_data TEXT                              -- Additional notes or miscellaneous data about the interviewer
+);
