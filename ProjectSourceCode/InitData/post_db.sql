9217bf82b9634cab319259cd059e7693d9788f81
Owen Witkos <owenwitkos@gmail.com>
2024-11-07 23:52:05 -0700

    added table for comments on posts

--- ProjectSourceCode/InitData/post_db.sql
+++ ProjectSourceCode/InitData/post_db.sql
@@ -7,6 +7,18 @@ CREATE TABLE posts
     upvotes INT,
     downvotes INT,
     time_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (username) REFERENCES users (username) ON DELETE CASCADE
+    FOREIGN KEY (username) REFERENCES users (username)
 );
 
+CREATE TABLE comments
+(
+    comment_id SERIAL PRIMARY KEY,
+    post_id INT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    body TEXT,
+    upvotes INT,
+    downvotes INT,
+    time_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (post_id) REFERENCES posts (post_id) ON DELETE CASCADE
+);
