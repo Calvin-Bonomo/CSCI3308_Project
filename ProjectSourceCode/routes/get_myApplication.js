c1e0bb5e28fc59ea5a427e86e01ff67eec4c9c7d
Jackson Hill <jahi7479@colorado.edu>
2024-11-22 01:32:37 -0700

    added most of the functionality for myApplication

--- ProjectSourceCode/routes/get_myApplication.js
+++ ProjectSourceCode/routes/get_myApplication.js
@@ -1,4 +1,6 @@
+const { Application } = require('../modules/application');
 const { PageContext } = require('../modules/page_context');
+const handlebars = require('handlebars');
 
 /**
  * entry point for the route module, this function is immediately called when
@@ -11,13 +13,34 @@ function main(app){
 
     // get handle to database from app
     /** @type {IDatabase} */
-    const database = app.database
+    const database = app.database;
+    const page = 1;
+    const page_size = 10;
 
-    // render myApplications page
+    // Register handlebars helpers
+    handlebars.registerHelper('formatDate', function(date) {
+        if (!date) return '';
+        return new Date(date).toLocaleDateString();
+    });
+
+    handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    });
+
+    // render myApplications page with applications data
     app.get('/my_applications', (req, res) => {
-        res.render('pages/myApplications', PageContext.Create(app, req));
-    })
+        Application.fetchAll(database, page_size, (page - 1) * page_size)
+            .then(applications => {
+                res.render('pages/myApplications', PageContext.Create(app, req, { applications }));
+            })
+            .catch(err => {
+                console.error('Failed to render my applications:', err);
+                res.status(400).send('An error occurred while fetching applications. Please try again later. Details: ' + err.message);
+            });
+    });
 }
 
 // export the specified entry point
 module.exports = main;
+
+
